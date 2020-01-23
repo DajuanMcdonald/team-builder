e120de8fcf54acfcc390ec624d09a8fc79a0aa75
fiopwk <flopwiki@gmail.com>
2020-01-22 19:38:31 -0500

    added new function to Form.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,6 +3,7 @@ import Form from './Form';
 
 
 function App() {
+
   return (
     <div className="App">
       <Form/>
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Form = props => {
+    //set the state
     const [person, setPerson] = useState({
         name: "",
         email: "",
@@ -8,6 +9,7 @@ const Form = props => {
 
     });
 
+    //Handle the form change event
     const handleChange = event => {
         setPerson({
             ...person, [event.target.name] : event.target.value
@@ -16,38 +18,62 @@ const Form = props => {
 
     const handleSubmit = event => {
         event.preventDefault();
+        const newPerson =  {
+            ...person, id: Date.now()
+        }
 
+    };
+
+    const addMember = member => {
+        setPerson({...person, member})
     };
     return (
         <div>
 
-        <form action="" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} >
+            {/*Name of Professional*/}
             <label htmlFor="name">Name:</label>
             <input
                 type="text"
+                placeholder="Enter a Name"
                 id="name"
                 name="name"
                 onChange={handleChange}
+                value={person.name}
             />
 
             <label htmlFor="email">Email:</label>
+            {/*Email of Professional*/}
             <input
                 type="email"
+                placeholder="Enter an Email"
                 id="email"
                 name="email"
                 onChange={handleChange}
+                value={person.email}
             />
+
+
 
             <label htmlFor="role">Role:</label>
-            <input
-                type="text"
-                id="role"
-                name="role"
-                onChange={handleChange}
-            />
+            {/*Role of Professional*/}
+            <select onChange={handleChange}>
+                <option value="Engineer">Engineer</option>
+                <option value="Developer">Developer</option>
+                <option value="Contributor">Contributor</option>
+                <option value="Consultant">Consultant</option>
+                <option value="Student">Student</option>
 
-            <input type="submit"
-                   value="Submit"
+            </select>
+
+
+
+
+            {/*Submit the data*/}
+            <input
+                onClick={addMember}
+                type="submit"
+                value="Submit"
             />
 
         </form>
